db81d41655990105976060c3ae9eff3557535c14
sai <2577519007@qq.com>
2017-11-24 15:12:19 +0800

    创建类文件

---
+++ play/play/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  play
+//
+//  Created by sai on 2017/11/24.
+//  Copyright © 2017年 BJYWT. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ play/play/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  play
+//
+//  Created by sai on 2017/11/24.
+//  Copyright © 2017年 BJYWT. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
