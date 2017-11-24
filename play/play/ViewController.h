13e76134ef602617b433550334107cd63f7a4022
sai <2577519007@qq.com>
2017-11-24 15:10:32 +0800

    初始化项目

---
+++ play/play/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  play
+//
+//  Created by sai on 2017/11/24.
+//  Copyright © 2017年 BJYWT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ play/play/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  play
+//
+//  Created by sai on 2017/11/24.
+//  Copyright © 2017年 BJYWT. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
